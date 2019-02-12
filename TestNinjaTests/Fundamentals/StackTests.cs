1ad4edccaf0d45e7626227ac6bb664ae0d488ad6
Guilherme de Jesus Santos <guiljs@gmail.com>
2019-02-12 21:52:55 -0200

    Stack tests

---
+++ TestNinjaTests/Fundamentals/StackTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Fundamentals.Tests
+{
+    [TestFixture()]
+    public class StackTests
+    {
+        [Test()]
+        public void Push_NullObject_ReturnArgumentNullException()
+        {
+            var stack = new Stack<int?>();
+
+            Assert.That(() =>
+            {
+                stack.Push(null);
+
+            }, Throws.ArgumentNullException);
+        }
+
+        [Test()]
+        public void Push_NewObject_ReturnListCountOne()
+        {
+            var stack = new Stack<int?>();
+
+            stack.Push(1);
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+
+        }
+
+        [Test()]
+        public void Pop_ListCountIsZero_ReturnInvalidOperationException()
+        {
+            var stack = new Stack<int?>();
+
+            Assert.That(() =>
+            {
+                stack.Pop();
+            }, Throws.InvalidOperationException);
+        }
+
+        [Test()]
+        public void Pop_ListCountIsGreaterThenZero_ReturnItemAndRemove()
+        {
+            var stack = new Stack<int?>();
+            stack.Push(1);
+
+            var result = stack.Pop();
+
+            Assert.That(result, Is.EqualTo(1));
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test()]
+        public void Peek_ListCountIsZero_ReturnInvalidOperationException()
+        {
+            var stack = new Stack<int?>();
+
+            Assert.That(() =>
+            {
+                stack.Peek();
+            }, Throws.InvalidOperationException);
+        }
+
+        [Test()]
+        public void Peek_ListCountIsGreaterThenZero_ReturnItemWithoutRemovingIt()
+        {
+            var stack = new Stack<int?>();
+            stack.Push(1);
+
+            var result = stack.Peek();
+
+            Assert.That(result, Is.EqualTo(1));
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+    }
+}
